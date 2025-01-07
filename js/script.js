4fa436ca5fbbf7fc1857e130fa12434c69935193
justyamzal <43919037+yamzal@users.noreply.github.com>
2025-01-07 15:47:35 +0700

    fixing gallery section

--- js/script.js
+++ js/script.js
@@ -71,21 +71,60 @@ const swiper = new Swiper(".swiper-container", {
   });
 
 //--- Gallery Popup ---//
-function showPopup (imageSrc,title,description) {
-    const popup = document.getElementById("popup");
-    const popupImg = document.getElementById("popup-img");
-    const popupTitle = document.getElementById("popup-title");
-    const popupDesc = document.getElementById("popup-desc");
+// function showPopup (imageSrc,title,description) {
+//     const popup = document.getElementById("popup");
+//     const popupImg = document.getElementById("popup-img");
+//     const popupTitle = document.getElementById("popup-title");
+//     const popupDesc = document.getElementById("popup-desc");
 
-    popupImg.src = imageSrc;
-    popupTitle.textContent = title;
-    popupDesc.textContent = description;
+//     popupImg.src = imageSrc;
+//     popupTitle.textContent = title;
+//     popupDesc.textContent = description;
     
-    popup.style.display = "flex";
-}
+//     popup.style.display = "flex";
+// }
 
-function hidePopup() {
-    const popup = document.getElementById("popup");
-    popup.style.display = "none";
-}
+// function hidePopup() {
+//     const popup = document.getElementById("popup");
+//     popup.style.display = "none";
+// }
 
+
+//--- Modal Overlay Popup ---//
+
+//Get all modal links 
+const modalLinks = document.querySelectorAll(".gallery-item a");
+
+//Add click event to each link
+modalLinks.forEach((link) => {
+  link.addEventListener("click", function (e){
+    e.preventDefault();
+    const modalId = this.getAttribute("data-modal");
+    const modal = document.getElementById(modalId);
+    if (modal) {
+      modal.style.display = "flex"; //show the modal 
+    }
+  });  
+});
+
+
+//Get all close buttons
+const closeButtons = document.querySelectorAll(".close-modal");
+
+//Add click event to close modals 
+closeButtons.forEach((button)=>{
+  button.addEventListener("click", function() {
+    this.closest(".modal-overlay").style.display = "none";
+  });
+});
+
+
+//Close modal when clicking outside content
+window.addEventListener("click", function(e) {
+  const modals = this.document.querySelectorAll(".modal-overlay");
+  modals.forEach((modal)=> {
+    if(e.target === modal) {
+      modal.style.display = "none";
+    }
+  });
+});
